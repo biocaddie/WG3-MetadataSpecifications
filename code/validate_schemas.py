f7f41494ae0d386bf81dc376598ed20bdfa68d06
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2016-05-19 17:20:59 +0100

    Updating process schemas

--- code/validate_schemas.py
+++ code/validate_schemas.py
@@ -8,8 +8,8 @@ from jsonschema import Draft4Validator
 path = os.path.abspath("../json-schemas")
 files = [ f for f in listdir(path) if isfile(join(path,f)) ]
 
-#schema = json.load(open(join("../json-schemas","study_schema.json")))
-#Draft4Validator.check_schema(schema)
+# schema = json.load(open(join("../json-schemas","study_schema.json")))
+# Draft4Validator.check_schema(schema)
 
 for schemaFile in files:
     print("Validating schema ", schemaFile, "...")
